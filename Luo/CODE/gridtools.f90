5ae4260ceab7b59959fc2cd8c570f9634393de2e
kbthomps <kyle.b.thompson@nasa.gov>
2016-02-17 15:59:09 -0500

    Begin modularizing old code

---
+++ Luo/CODE/gridtools.f90
@@ -0,0 +1,137 @@
+module gridtools
+
+  implicit none
+  private
+
+  public :: readgrid_lou
+  public :: basis_function
+  public :: face_norm
+
+contains
+
+!==========================GRID FILE READ======================================
+  subroutine readgrid_lou(ndimn,ntype,nelem,npoin,nface,nvertices,inpoel,     &
+                          gridfile,coord,bcface)
+  
+  integer      :: titleline,ndimn,ntype,nelem,npoin,nface,nvertices
+  integer      :: i,j,elem_dummy,poin_dummy,face_dummy
+  
+  real(8), dimension(:,:),allocatable  :: coord
+  
+  integer, dimension(:,:), allocatable :: inpoel,bcface
+  
+  character(len=100) :: gridfile
+  
+  open(12,file=gridfile,status="old")
+  read(12,*) titleline
+  
+  do i=1,titleline
+    read(12,*)
+  end do
+  
+  read(12,*) 
+  read(12,*) ndimn,ntype
+  read(12,*)
+  read(12,*) nelem,npoin,nface
+  read(12,*)
+  
+  allocate(inpoel(3,nelem))
+  allocate(coord(ndimn,npoin))
+  allocate(bcface(ndimn+1,nface))
+  
+  do j=1,nelem
+    read(12,*) elem_dummy,(inpoel(i,j),i=1,nvertices)
+  end do
+  
+  read(12,*)
+  
+  do j=1,npoin
+    read(12,*) poin_dummy,(coord(i,j),i=1,ndimn)
+  end do
+  
+  do j=1,npoin+2
+    read(12,*)
+  end do
+  
+  do j=1,nface
+    read(12,*) face_dummy,(bcface(i,j),i=1,ndimn+1)
+  end do
+  
+  end subroutine
+
+!========================GEOMETRY FACES=========================================
+  subroutine basis_function(nelem,geoel,inpoel,coord)
+  
+  integer    :: i,j,ielem,nelem,ip1,ip2,ip3
+  
+  real(8)    :: D,dd
+  
+  real(8), dimension(:),   allocatable :: a,b
+  real(8), dimension(:,:), allocatable :: geoel,coord
+  
+  integer, dimension(:,:), allocatable :: inpoel
+  
+  allocate(a(3))
+  allocate(b(3))
+  allocate(geoel(5,nelem))
+  
+  do ielem=1,nelem
+    
+    ip1  = inpoel(1,ielem)
+    ip2  = inpoel(2,ielem)
+    ip3  = inpoel(3,ielem)
+  
+    a(1) = coord(2,ip2)-coord(2,ip3)
+    a(2) = coord(2,ip3)-coord(2,ip1)
+  
+    b(1) = -(coord(1,ip2)-coord(1,ip3))
+    b(2) = -(coord(1,ip3)-coord(1,ip1))
+  
+    D    = a(1)*b(2)-a(2)*b(1)
+    dd   = 1/D
+  
+    geoel(5,ielem) = D
+  
+    a(1) = a(1)*dd
+    a(2) = a(2)*dd
+    b(1) = b(1)*dd
+    b(2) = b(2)*dd
+  
+    geoel(1,ielem) = a(1)
+    geoel(2,ielem) = a(2)
+    geoel(3,ielem) = b(1)
+    geoel(4,ielem) = b(2)
+  
+  end do
+  
+  end subroutine
+  
+  !==========================CALCULATE NORMAL VECTORS============================
+  
+  subroutine face_norm(rface,coord,bcface,nface,ndimn,npoin)
+  
+  real(8)  x1,x2,y1,y2,coord(ndimn,npoin)
+  
+  real(8), dimension(:,:), allocatable :: rface
+  
+  integer  bcface(ndimn+1,nface),ip1,ip2,iface,ndimn,npoin,nface
+  
+  allocate(rface(ndimn,nface))
+  
+  do iface=1,nface
+    ip1  = bcface(1,iface)
+    ip2  = bcface(2,iface)
+    
+    x1   = coord(1,ip1)
+    x2   = coord(1,ip2)
+  
+    y1   = coord(2,ip1)
+    y2   = coord(2,ip2)
+    
+    rface(1,iface) = y2-y1
+    rface(2,iface) = x1-x2
+  end do
+  
+  end subroutine
+
+end module gridtools
--- Luo/CODE/main.f90
+++ Luo/CODE/main.f90
@@ -1,3 +1,7 @@
+program main
+
+  use gridtools, only : readgrid_lou, basis_function, face_norm
+
 implicit none
 
 !==============================================================================
@@ -40,17 +44,11 @@ close(11)
 
 open(30,file="trouble.dat",status="replace")
 
-!==============================================================================
-!                        READ IN GRID FILE
-!==============================================================================
-
+! Read the grid
 call readgrid_lou(ndimn,ntype,nelem,npoin,nface,nnode,inpoel,         &
                   gridfile,coord,bcface)
 
-!==============================================================================
-!                        ASSEMBLE GEOMETRY MATRICIES
-!==============================================================================
-
+! assemble geometry related matricies
 call basis_function(nelem,geoel,inpoel,coord)
 
 !=============================================================================
@@ -97,10 +95,7 @@ end do
 close(34)
 close(35)
 
-!==============================================================================
-!                     FORMULATE THE LOAD VECTOR
-!==============================================================================
-
+! Formulate the load vector
 call face_norm(rface,coord,bcface,nface,ndimn,npoin)
 call rhslap(bcface,rface,uinf,vinf,rhspo)
 
@@ -199,53 +194,6 @@ do ipoin=1,npoin
   Vt(ipoin) = sqrt(Vx(ipoin)**2 + Vy(ipoin)**2)
 
 end do
- 
-!==============================================================================
-!                        EXACT SOLUTION (CYLINDER)
-!==============================================================================
-
-if(trim(bc_case) == "cylinder") then
-
-  allocate(phi_exact(npoin))
-  allocate(phidiff(npoin))
-  allocate(V_exact(npoin))
-  allocate(v_diff(npoin))
-
-  open(31,file=comp_name,status="replace")
-
-  do ipoin=1,npoin
-    call cylpot(coord,npoin,phi_exact(ipoin),V_exact(ipoin),ipoin)
-  end do
-   
-  avg_mesh = 0.0
-  
-  do i=1,nelem
-    avg_mesh = avg_mesh+geoel(5,i)
-  end do
-  
-  avg_mesh=sqrt(avg_mesh/nelem)
-  
-  write(31,*) "Average Mesh Spacing = ",avg_mesh
- 
-  L2 = 0.0
-
-  do i=1,npoin
-    phidiff(i) = phi(i)-phi_exact(i)
-    v_diff(i)   = Vt(i)-V_exact(i)
-!   write(31,*) phi(i),phi_exact(i),phidiff(i)
-    L2   = L2   + phidiff(i)**2
-    L2_v = L2_v + v_diff(i)**2
-  end do
-
-  L2   = sqrt(L2)
-  L2_v = sqrt(L2_v)
-
-  write(31,*) "the L2 Error Norm = ",L2
-  write(31,*) "the L2 velocity Error Norm = ",L2_v
-
-  close(31)
-
-end if
 
 !==============================================================================
 !                        TECPLOT OUTPUT
@@ -278,27 +226,6 @@ end do
 
 close(21)
 close(30)
-
-if(trim(bc_case)=="cylinder") then
-
-open(15,file="cylinder_surface.dat",status='replace')
-
-write(15,*) 'TITLE = "',trim(tec_dataname),'"'
-write(15,*) 'VARIABLES = "X" "Y" "Vt"'
-
-do i=1,npoin
-  if(bcface(3,i)==2) then
-    ip1=bcface(1,i)
-    ip2=bcface(2,i)
-    
-    write(15,*) coord(1,ip1),coord(2,ip1),Vt(ip1)
-    write(15,*) coord(1,ip2),coord(2,ip2),Vt(ip2)
-  end if
-end do
-    
-close(15)
-
-end if
 
 if(trim(bc_case)=="channel") then
 
@@ -335,132 +262,6 @@ end if
 !==============================================================================
 
 contains
-
-!==========================GRID FILE READ======================================
-subroutine readgrid_lou(ndimn,ntype,nelem,npoin,nface,nvertices,inpoel,     &
-                        gridfile,coord,bcface)
-
-integer      :: titleline,ndimn,ntype,nelem,npoin,nface,nvertices
-integer      :: i,j,elem_dummy,poin_dummy,face_dummy
-
-real(8), dimension(:,:),allocatable  :: coord
-
-integer, dimension(:,:), allocatable :: inpoel,bcface
-
-character(len=100) :: gridfile
-
-open(12,file=gridfile,status="old")
-read(12,*) titleline
-
-do i=1,titleline
-  read(12,*)
-end do
-
-read(12,*) 
-read(12,*) ndimn,ntype
-read(12,*)
-read(12,*) nelem,npoin,nface
-read(12,*)
-
-allocate(inpoel(3,nelem))
-allocate(coord(ndimn,npoin))
-allocate(bcface(ndimn+1,nface))
-
-do j=1,nelem
-  read(12,*) elem_dummy,(inpoel(i,j),i=1,nvertices)
-end do
-
-read(12,*)
-
-do j=1,npoin
-  read(12,*) poin_dummy,(coord(i,j),i=1,ndimn)
-end do
-
-do j=1,npoin+2
-  read(12,*)
-end do
-
-do j=1,nface
-  read(12,*) face_dummy,(bcface(i,j),i=1,ndimn+1)
-end do
-
-end subroutine
-
-
-!========================GEOMETRY FACES=========================================
-subroutine basis_function(nelem,geoel,inpoel,coord)
-
-integer    :: i,j,ielem,nelem,ip1,ip2,ip3
-
-real(8)    :: D,dd
-
-real(8), dimension(:),   allocatable :: a,b
-real(8), dimension(:,:), allocatable :: geoel,coord
-
-integer, dimension(:,:), allocatable :: inpoel
-
-allocate(a(3))
-allocate(b(3))
-allocate(geoel(5,nelem))
-
-do ielem=1,nelem
-  
-  ip1  = inpoel(1,ielem)
-  ip2  = inpoel(2,ielem)
-  ip3  = inpoel(3,ielem)
-
-  a(1) = coord(2,ip2)-coord(2,ip3)
-  a(2) = coord(2,ip3)-coord(2,ip1)
-
-  b(1) = -(coord(1,ip2)-coord(1,ip3))
-  b(2) = -(coord(1,ip3)-coord(1,ip1))
-
-  D    = a(1)*b(2)-a(2)*b(1)
-  dd   = 1/D
-
-  geoel(5,ielem) = D
-
-  a(1) = a(1)*dd
-  a(2) = a(2)*dd
-  b(1) = b(1)*dd
-  b(2) = b(2)*dd
-
-  geoel(1,ielem) = a(1)
-  geoel(2,ielem) = a(2)
-  geoel(3,ielem) = b(1)
-  geoel(4,ielem) = b(2)
-
-end do
-
-end subroutine
-
-!==========================CALCULATE NORMAL VECTORS============================
-
-subroutine face_norm(rface,coord,bcface,nface,ndimn,npoin)
-
-real(8)  x1,x2,y1,y2,coord(ndimn,npoin)
-
-real(8), dimension(:,:), allocatable :: rface
-
-integer  bcface(ndimn+1,nface),ip1,ip2,iface,ndimn,npoin,nface
-
-allocate(rface(ndimn,nface))
-
-do iface=1,nface
-  ip1  = bcface(1,iface)
-  ip2  = bcface(2,iface)
-  
-  x1   = coord(1,ip1)
-  x2   = coord(1,ip2)
-
-  y1   = coord(2,ip1)
-  y2   = coord(2,ip2)
-  
-  rface(1,iface) = y2-y1
-  rface(2,iface) = x1-x2
-end do
-
-end subroutine
 
 !===============================LOAD VECTOR====================================
 subroutine rhslap(bcface,rface,uinf,vinf,rhspo)
@@ -711,4 +512,4 @@ end subroutine
 
    12 END SUBROUTINE
 
-end
+end program main
