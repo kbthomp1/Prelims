e76ad333da280f7e8835f5cacc742a5887536bc9
Kyle Thompson <kbthomps@ncsu.edu>
2016-02-22 22:31:45 -0500

    Save CG_P1 code and start work on new data struc

--- Luo/CODE/CG_P1/main.f90
+++ Luo/CODE/CG_P1/main.f90
@@ -5,16 +5,15 @@ program main
   use solver,        only : get_lhspo, get_rhspo, set_bc, get_soln, solve
   use io_helpers,    only : write_tec_volume, write_tec_surface, read_namelist
   use namelist_data, only : uinf, vinf, gridfile, nnode, uinf, vinf, nsteps,   &
-                            nvars, neqns, tec_dataname, lin_solver, tolerance
+                            tec_dataname, lin_solver, tolerance
   implicit none
   
   integer :: ndimn, ntype, nelem, npoin, nface
   
+  real(dp), dimension(:),   allocatable :: rhspo, phi
   real(dp), dimension(:),   allocatable :: Vx, Vy, Vt
   
-  real(dp), dimension(:,:),     allocatable :: coord, geoel, rface
-  real(dp), dimension(:,:),     allocatable :: rhspo, phi
-  real(dp), dimension(:,:,:,:), allocatable :: lhspo
+  real(dp), dimension(:,:), allocatable :: coord, geoel, lhspo, rface
   
   integer, dimension(:,:),  allocatable :: inpoel, bcface
 
@@ -27,9 +26,9 @@ continue
                     coord,bcface)
   
 ! Allocate the work arrays
-  allocate(lhspo(nvars,neqns,npoin,npoin))
-  allocate(rhspo(nvars,npoin))
-  allocate(phi(nvars,npoin))
+  allocate(lhspo(npoin,npoin))
+  allocate(rhspo(npoin))
+  allocate(phi(npoin))
   allocate(Vx(npoin))
   allocate(Vy(npoin))
   allocate(Vt(npoin))
@@ -39,14 +38,14 @@ continue
   call face_norm(rface,coord,bcface,nface,ndimn)
   
 ! Formulate the load vector (RHS)
-  rhspo = get_rhspo(bcface,rface,nface,npoin,neqns,uinf,vinf)
+  rhspo = get_rhspo(bcface,rface,nface,npoin,uinf,vinf)
   
 ! Formulate the stiffness matrix (LHS)
-  lhspo = get_lhspo(npoin,nelem,nnode,nvars,neqns,inpoel,geoel)
+  lhspo = get_lhspo(npoin,nelem,nnode,inpoel,geoel)
    
-  call set_bc(phi,lhspo,rhspo,npoin,nvars,bcface)
+  call set_bc(phi,lhspo,rhspo,npoin,bcface)
   
-  call solve(lin_solver,lhspo,rhspo,phi,npoin,nvars,neqns,nsteps,tolerance)
+  call solve(lin_solver,lhspo,rhspo,phi,npoin,nsteps,tolerance)
   
   call get_soln(Vx,Vy,Vt,phi,geoel,inpoel,npoin,nelem)
   
--- Luo/CODE/CG_P1/namelist_data.f90
+++ Luo/CODE/CG_P1/namelist_data.f90
@@ -10,7 +10,7 @@ module namelist_data
   private
 
   public :: uinf, vinf, tolerance
-  public :: nsteps, nnode, nvars, neqns
+  public :: nsteps, nnode
   public :: gridfile, tec_dataname, lin_solver
 
   real(dp) :: uinf
@@ -19,8 +19,6 @@ module namelist_data
 
   integer :: nsteps
   integer :: nnode
-  integer :: nvars !Number of unknowns to solve
-  integer :: neqns !Number of equations
 
   character(len=100) :: gridfile
   character(len=100) :: tec_dataname
