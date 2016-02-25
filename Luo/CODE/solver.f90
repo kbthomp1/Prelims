1e6d18f0c87e389e05a4725723a54303633c46d3
kbthomps <kyle.b.thompson@nasa.gov>
2016-02-25 10:10:39 -0500

    Added informative comments and refactored
    o Detailed explanation of how the gradients are averaged to the nodes
    o Fixed area weighting to truly be area weighting
    o Got rid of ridiculous arrays storing temp values

--- Luo/CODE/solver.f90
+++ Luo/CODE/solver.f90
@@ -112,6 +112,15 @@ contains
 
 !============================ GET_SOLN ======================================80
 ! Calculate velocities Vx, Vy, and Vt by interpolating solution to nodes
+!
+! Note that the gradients of the linear basis functions are constant across
+! each element, thus we can use an area weighted average to recover the 
+! the gradient at a node.  Since phi = sum(phi_i*B_i)
+! 
+!   phi_x = sum(phi_i*Bx_i) = phi_1*a_1 + phi_2*a_2 + phi_3*a_3
+!   phi_y = sum(phi_i*By_i) = phi_1*b_1 + phi_2*b_2 + phi_3*b_3
+! 
+! NOTE: since B_1 + B_2 + B_3 = 1, a/b_3 = 1 - a/b_2 - a/b_1 
 !============================================================================80
 
   subroutine get_soln(Vx,Vy,Vt,phi,grid)
@@ -121,12 +130,14 @@ contains
     real(dp), dimension(:),   intent(in)  :: phi
     real(dp), dimension(:),   intent(out) :: Vx, Vy, Vt
 
-    real(dp), dimension(grid%nelem) :: Vx_local, Vy_local
     real(dp), dimension(grid%npoin) :: Vxarea, Vyarea, area
 
     integer :: ielem, ipoin, ip1, ip2, ip3
 
+    real(dp) :: local_area, Vx_local, Vy_local
+
     real(dp), parameter :: zero = 0.0_dp
+    real(dp), parameter :: half = 0.5_dp
 
   continue
 
@@ -142,25 +153,27 @@ contains
       ip2=grid%inpoel(2,ielem)
       ip3=grid%inpoel(3,ielem)
 
-      Vx_local(ielem) = (grid%geoel(1,ielem)*(phi(ip1)-phi(ip3))       &
-                       + grid%geoel(2,ielem)*(phi(ip2)-phi(ip3)))      &
-                       * grid%geoel(5,ielem)
+      local_area = half*grid%geoel(5,ielem)
 
-      Vy_local(ielem) = (grid%geoel(3,ielem)*(phi(ip1)-phi(ip3))       &
-                       + grid%geoel(4,ielem)*(phi(ip2)-phi(ip3)))      &
-                       * grid%geoel(5,ielem)
+      Vx_local = (grid%geoel(1,ielem)*(phi(ip1)-phi(ip3))       &
+                + grid%geoel(2,ielem)*(phi(ip2)-phi(ip3)))      &
+                * local_area
 
-      Vxarea(ip1) = Vxarea(ip1)+Vx_local(ielem)
-      Vxarea(ip2) = Vxarea(ip2)+Vx_local(ielem)
-      Vxarea(ip3) = Vxarea(ip3)+Vx_local(ielem)
+      Vy_local = (grid%geoel(3,ielem)*(phi(ip1)-phi(ip3))       &
+                + grid%geoel(4,ielem)*(phi(ip2)-phi(ip3)))      &
+                * local_area
 
-      Vyarea(ip1) = Vyarea(ip1)+Vy_local(ielem)
-      Vyarea(ip2) = Vyarea(ip2)+Vy_local(ielem)
-      Vyarea(ip3) = Vyarea(ip3)+Vy_local(ielem)
+      Vxarea(ip1) = Vxarea(ip1)+Vx_local
+      Vxarea(ip2) = Vxarea(ip2)+Vx_local
+      Vxarea(ip3) = Vxarea(ip3)+Vx_local
 
-      area(ip1)  = area(ip1)+grid%geoel(5,ielem)
-      area(ip2)  = area(ip2)+grid%geoel(5,ielem)
-      area(ip3)  = area(ip3)+grid%geoel(5,ielem)
+      Vyarea(ip1) = Vyarea(ip1)+Vy_local
+      Vyarea(ip2) = Vyarea(ip2)+Vy_local
+      Vyarea(ip3) = Vyarea(ip3)+Vy_local
+
+      area(ip1)  = area(ip1)+local_area
+      area(ip2)  = area(ip2)+local_area
+      area(ip3)  = area(ip3)+local_area
 
     end do
 
